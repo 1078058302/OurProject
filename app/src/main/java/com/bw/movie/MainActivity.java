b68f59814fc5cbc048c1e16d5486731f149a34e1
ZhangJinQiang <43294355+zhangjinqiang61@users.noreply.github.com>
2018-11-27 11:04:46 +0800

    AAA

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -3,12 +3,12 @@ package com.bw.movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //主Activity
+//Activity
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
