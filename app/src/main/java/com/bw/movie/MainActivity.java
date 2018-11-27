0e1b9f630ca488e891e06aa2f09903b2e7cf0340
fanzh <fanzhuopu@126.com>
2018-11-27 10:19:11 +0800

    test

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -1,8 +1,7 @@
-package com.umeng.soexample;
+package com.bw.movie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 
 public class MainActivity extends AppCompatActivity {
 
