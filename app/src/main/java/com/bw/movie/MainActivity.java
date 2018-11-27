aaca6d0c35ed2cd1e9cde7fb66d5e73c4952bb02
fanzh <fanzhuopu@126.com>
2018-11-27 11:01:38 +0800

    add zhu

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -2,13 +2,13 @@ package com.bw.movie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//主Activity
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //AAAAAAAAAAAAA
+
     }
 }
